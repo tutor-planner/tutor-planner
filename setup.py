c0310ee540c5578646527d41ab2e7f1e71ed986b
Alexander Elvers <aelvers@inet.tu-berlin.de>
2017-10-26 16:03:55 +0200

    v1.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ dev_requires = [
 
 setup(
     name="tutor-planner",
-    # version="1.0a1",
+    version="1.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=install_requires,
